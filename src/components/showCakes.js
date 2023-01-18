16f8720633f18f1c6360d14d4dcabe22d4f509ff
Fulya <fulya.ertay@gmail.com>
2023-01-18 13:22:37 +0300

    added text style

--- src/components/showCakes.js
+++ src/components/showCakes.js
@@ -9,13 +9,13 @@ const [selectedImg,setSelectedImg]=useState(Images[0]);
 
   return (
     <div className="App  bg-gray-200 flex w-full mx-auto m-5 p-10 justify-center items-center flex-col">
-      <h1 className='text-2xl text-purple-900'>Happy Birthday!❤️</h1>
+      <h1 className='font-extrabold text-transparent md:text-4xl sm:text-sm bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600'>Happy Birthday!❤️</h1>
       <div className="container w-full w-max-width-[540px] mx-auto m-5 justify-center">
         <img src={selectedImg} alt="Selected" className="selected mx-auto md:w-[400px] rounded md:h-[400px] sm:w-[200px] md:h-[200px] object-cover"/>
       </div>
-      <div className="imgContainer flex justify-center w-70 h-auto grid grid-cols-3 gap-4 object-cover m-10 ">
+      <div className="imgContainer flex justify-center w-70 h-auto grid grid-cols-3 gap-4 m-10 ">
         {Images.map((img,index) =>(
-          <img key={index} src={img} alt='birthday-cake' className='md:w-[200px] md:h-[200px] sm:w-[50px] sm:h-[50px] md:block md:cursor-pointer border border-2'
+          <img key={index} src={img} alt='birthday-cake' className='md:w-[200px] md:h-[200px] sm:w-[50px] sm:h-[50px] md:block md:cursor-pointer object-cover  border border-2'
           onClick={() => setSelectedImg(img)}
           />
         ))}
