0a1917d564d0c200778f4c85abd0244b0df99f82
Fulya <fulya.ertay@gmail.com>
2023-01-18 13:05:32 +0300

    added  responsive styles

--- src/components/showCakes.js
+++ src/components/showCakes.js
@@ -11,11 +11,11 @@ const [selectedImg,setSelectedImg]=useState(Images[0]);
     <div className="App  bg-gray-200 flex w-full mx-auto m-5 p-10 justify-center items-center flex-col">
       <h1 className='text-2xl'>Happy Birthday!</h1>
       <div className="container w-full w-max-width-[540px] mx-auto m-5 justify-center border border-4">
-        <img src={selectedImg} alt="Selected" className="selected mx-auto w-[400px] h-[400px] object-cover"/>
+        <img src={selectedImg} alt="Selected" className="selected mx-auto md:w-[400px] md:h-[400px] sm:w-[200px] md:h-[200px] object-cover"/>
       </div>
       <div className="imgContainer flex justify-center w-70 h-auto grid grid-cols-3 gap-4 object-cover m-10 ">
         {Images.map((img,index) =>(
-          <img key={index} src={img} alt='birthday-cake' className='w-[200px] h-[200px] block cursor-pointer border border-2'
+          <img key={index} src={img} alt='birthday-cake' className='md:w-[200px] md:h-[200px] sm:w-[50px] sm:h-[50px] md:block md:cursor-pointer border border-2'
           onClick={() => setSelectedImg(img)}
           />
         ))}
