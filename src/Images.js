103d823ac150a798d456dc537859f2a532f709b2
Fulya <fulya.ertay@gmail.com>
2023-01-18 13:11:38 +0300

    chanege image

--- src/Images.js
+++ src/Images.js
@@ -2,7 +2,7 @@ export default[
     "https://i.pinimg.com/originals/6c/53/03/6c5303fb41a4111e6ae4b2546587e71b.jpg",
     "https://i.pinimg.com/originals/3e/ec/8e/3eec8e431d58cf4ae94511492395d401.jpg",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLMc8goBnw-D3m9cuTJBgEtvDzafm5FbDwFA&usqp=CAU",
-    "https://www.coolest-birthday-cakes.com/files/2015/06/a-computer-engineers-dream-cake-75386-e1435094826689-480x338.jpg",
+    "https://i.pinimg.com/736x/a0/16/92/a0169234c3a2cedb7c47611159efe0a6--ecommerce-marketing-clouds.jpg",
     "https://preview.redd.it/nl6eg4b09t991.jpg?width=640&crop=smart&auto=webp&s=2b6673c00b5a18582a9e894de039d77b8a870af5",
     "https://thesmartcookiecook.com/wp-content/uploads/2022/07/21st-birthday-cake-for-boy-3398.jpg"
 ]
--- src/components/showCakes.js
+++ src/components/showCakes.js
@@ -9,9 +9,9 @@ const [selectedImg,setSelectedImg]=useState(Images[0]);
 
   return (
     <div className="App  bg-gray-200 flex w-full mx-auto m-5 p-10 justify-center items-center flex-col">
-      <h1 className='text-2xl'>Happy Birthday!</h1>
-      <div className="container w-full w-max-width-[540px] mx-auto m-5 justify-center border border-4">
-        <img src={selectedImg} alt="Selected" className="selected mx-auto md:w-[400px] md:h-[400px] sm:w-[200px] md:h-[200px] object-cover"/>
+      <h1 className='text-2xl text-purple-900'>Happy Birthday!❤️</h1>
+      <div className="container w-full w-max-width-[540px] mx-auto m-5 justify-center">
+        <img src={selectedImg} alt="Selected" className="selected mx-auto md:w-[400px] rounded md:h-[400px] sm:w-[200px] md:h-[200px] object-cover"/>
       </div>
       <div className="imgContainer flex justify-center w-70 h-auto grid grid-cols-3 gap-4 object-cover m-10 ">
         {Images.map((img,index) =>(
