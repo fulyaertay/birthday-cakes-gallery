29f243418083e303ca2de1a8a6c6d4cfde89c0c8
Fulya <fulya.ertay@gmail.com>
2023-01-18 12:47:39 +0300

    feat:added styles

---
+++ src/Images.js
@@ -0,0 +1,7 @@
+export default[
+    "https://i.ytimg.com/vi/XekZ6QYYtnQ/hqdefault.jpg",
+    "https://i.pinimg.com/originals/3e/ec/8e/3eec8e431d58cf4ae94511492395d401.jpg",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLMc8goBnw-D3m9cuTJBgEtvDzafm5FbDwFA&usqp=CAU",
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqAhbC6BNJ3DCUQZwj9kAjhpTx3ZJYxs8XBQ&usqp=CAU",
+    "https://preview.redd.it/nl6eg4b09t991.jpg?width=640&crop=smart&auto=webp&s=2b6673c00b5a18582a9e894de039d77b8a870af5"
+]
